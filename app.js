3e448ee7ac43aeddf5565f3ba695973169fdad39
tepavcevic <djordje.tepa@gmail.com>
2023-03-02 10:56:53 +0100

    Refactored to adding "root" div by creating it in JS

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const createMessage = (msg) => {
-const root = document.getElementById("root");
+const root = document.createElement("div");
+root.setAttribute("id", "root");
+
+document.body.appendChild(root);
 
 const message = document.createElement("h1");
 message.innerText = msg;
