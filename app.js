3931b382e6e8f09adcd406141fd8d7728c8ccf30
tepavcevic <djordje.tepa@gmail.com>
2023-03-02 11:07:10 +0100

    Fixed appending bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createMessage = (msg) => {
     const message = document.createElement("h1");
     message.innerText = msg;
 
-    root.appendChild(message);
+    document.body.appendChild(message);
 }
 
 createMessage("Hello World");
