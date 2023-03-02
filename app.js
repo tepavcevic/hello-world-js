cb0e61e2511748c3efbc6289a098eac863986fd3
tepavcevic <djordje.tepa@gmail.com>
2023-03-02 11:05:47 +0100

    Appending to body and code formatting changes

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const createMessage = (msg) => {
-const root = document.createElement("div");
-root.setAttribute("id", "root");
+    const message = document.createElement("h1");
+    message.innerText = msg;
 
-document.body.appendChild(root);
-
-const message = document.createElement("h1");
-message.innerText = msg;
-
-root.appendChild(message)
+    root.appendChild(message);
 }
 
 createMessage("Hello World");
